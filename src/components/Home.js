445c7784e8ae8c6c3fba7c10b3f74293de7dbeda
matiasbenavide <matiasbenavide@gmail.com>
2021-08-09 20:20:51 -0300

    optimización Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,94 +1,22 @@
 import React from 'react';
+import CounterCard from './CounterCard';
+import CounterCategory from './CounterCategory';
 import LatestProduct from './LatestProduct';
 import LatestUser from './LatestUser';
+import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function Home() {
-    /*==>PRODUCTS HOOK<==*/
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        console.log('%cse montó el componente', 'color: green');
-        fetch('http://localhost:3030/api/products')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.meta);
-                setProducts(data.meta)
-            })
-            .catch(error => console.log(error))
-    }, [])
-
-    useEffect(() => {
-        console.log('%cse actualizó el componente', 'color: yellow');
-    }, [products])
-
-    useEffect(() => {
-        console.log('%cse desmontó el componente', 'color: red');
-    }, [])
-
-    /*==><==*/
-    const [categories, setCategories] = useState([]);
-
-    useEffect(() => {
-        console.log('%cse montó el componente', 'color: green');
-        fetch('http://localhost:3030/api/products')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.meta.countByCategory);
-                setCategories(data.meta.countByCategory)
-            })
-            .catch(error => console.log(error))
-    }, [])
-
-    useEffect(() => {
-        console.log('%cse actualizó el componente', 'color: yellow');
-    }, [categories])
-
-    useEffect(() => {
-        console.log('%cse desmontó el componente', 'color: red');
-    }, [])
-
-    /*==>USERS HOOK<==*/ 
-    const [users, setUsers] = useState([])
-
-    useEffect(() => {
-        console.log('%cse montó el componente', 'color: green');
-        fetch('http://localhost:3030/api/users')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.meta);
-                setUsers(data.meta)
-            })
-            .catch(error => console.log(error))
-    }, [])
-
-    useEffect(() => {
-        console.log('%cse actualizó el componente', 'color: yellow');
-    }, [users])
-
-    useEffect(() => {
-        console.log('%cse desmontó el componente', 'color: red');
-    }, [])
-
     return(
         <React.Fragment>
             <div className="container">
                 <h1 className="">Dashboard Peritroopers</h1>
-                <ul>
-                    <li><div>Usuarios en lista: {users.count}</div></li>
-                    <li>Productos en lista: {products.count}</li>
-                    <li>
-                        Total por Categoría:
-                        <ul>
-                            <li>Teclados: {categories.Teclados}</li>
-                            <li>Auriculares: {categories.Aurículares}</li>
-                            <li>Mouses: {categories.Mouses}</li>
-                            <li>Micrófonos: {categories.Micrófonos}</li>
-                        </ul>
-                    </li>
-                </ul>
+                <CounterCard/>
+                <br/><br/>
+                <CounterCategory/>
+                <br/><br/>
                 <LatestProduct/>
-                <br/>
+                <br/><br/>
                 <LatestUser/>
             </div>
         </React.Fragment>
